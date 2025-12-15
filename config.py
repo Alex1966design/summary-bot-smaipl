71694349e187f13d9e41fb63d902904656a56f33
Alex1966design <a.kamenetsky@mail.ru>
2025-12-15 17:57:53 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+TELEGRAM_BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+SMAIPL_API_URL = os.environ["SMAIPL_API_URL"]
+SMAIPL_BOT_ID = int(os.environ["SMAIPL_BOT_ID"])
+SUMMARY_COMMAND = "/summary"
