787ec0dcd273a579e6821f1f379a29a2ea69a16f
Alex1966design <a.kamenetsky@mail.ru>
2025-12-15 18:50:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 TELEGRAM_BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
-SMAIPL_API_URL = os.environ["SMAIPL_API_URL"]
-SMAIPL_BOT_ID = int(os.environ["SMAIPL_BOT_ID"])
-SUMMARY_COMMAND = "/summary"
+SMAIPL_API_URL = os.environ["SMAIPL_API_URL"]           # полный URL вида https://api.smaipl.ru/api/v1.0/ask/<token>
+SMAIPL_BOT_ID = int(os.environ.get("SMAIPL_BOT_ID", "5129"))
+SUMMARY_COMMAND = os.environ.get("SUMMARY_COMMAND", "/summary")
+
